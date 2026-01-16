ff9ba9c180ddc11b049cb36aa743da8514807bb1
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-01-16 19:07:22 -0300

    csharp: new

---
+++ csharp/Exercise.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Exercise
+{
+    public static void Exe()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,38 +1,19 @@
 ﻿using System;
+using System.Security.Cryptography;
 
-class Program
+namespace csharp
+{
+    class Program
 {
     static void Main()
     {
-      int a = 1;
-      int b = 2;
+        string age = "2"; 
+        int idade;
 
-      for (int i = 1; i < 11; i++)   
-        {
-            if (i == 1 || i == 2)
-            {
-                Console.WriteLine($"Tabuada {a}x{i} = {i*a}"); 
-            }
-            
-            if (i == 10)
-            {
-                Console.WriteLine("---");
-                Console.WriteLine($"Tabuada {a}x{i} = {i*a}"); 
-            }
-        }            
-        Console.WriteLine("");
-        for (int y = 1; y < 3; y++) 
-        {
-            Console.WriteLine($"Tabuada {b}x{y} = {y*b}");    
-        }
-        Console.WriteLine("---");
-        for (int z = 10; z != 0; z--) 
-        {
-            if (z == 10)
-            {
-                Console.WriteLine($"Tabuada {b}x{z} = {z*b}");
-            }
-                
+        int.TryParse(age, out idade);
+        
         }
     }
 }
+
+
